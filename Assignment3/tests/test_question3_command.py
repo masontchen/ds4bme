222f6452149c6d569cd79e41a59b93a3b055e6be
Mason Tianyi Chen <39594659+masontchen@users.noreply.github.com>
2021-02-18 13:18:06 -0500

    Create test_question3_command.py

---
+++ Assignment3/tests/test_question3_command.py
@@ -0,0 +1,3 @@
+stopifnot(sum(grep("readline", readLines("question3.R"))) > 0)
+stopifnot(sum(grep("prompt", readLines("question3.R"))) > 0)
+stopifnot(sum(grep("if", readLines("question3.R"))) > 0)
