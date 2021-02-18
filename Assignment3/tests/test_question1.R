db6ece5832a74e99fa02a450f834b6b616180f3f
Mason Tianyi Chen <39594659+masontchen@users.noreply.github.com>
2021-02-17 19:43:22 -0500

    Update and rename test_question1.R to test_question1_matrix.R

--- Assignment3/tests/test_question1.R
+++
@@ -1,8 +0,0 @@
-source("question1.R")
-
-res1 = mylag(c(1, 5, 8, 4), c(1, 3))
-gt = NA
-
-stopifnot(abs(intercept - ground_truth[1]) < 1e-10)
-stopifnot(abs(coeff1 - ground_truth[2]) < 1e-10)
-stopifnot(abs(coeff2 - ground_truth[3]) < 1e-10)
---
+++ Assignment3/tests/test_question1_matrix.R
@@ -0,0 +1,15 @@
+source("question1.R")
+
+res1 = mylag(c(1, 5, 8, 4), c(1, 3))
+gt1 = matrix(c(1, 5, 8, 4, NA, 1, 5, 8, NA, NA, NA, 1), 4, 3)
+
+res2 = mylag(c(1, 5, 8, 4), c(-1, -2))
+gt2 = matrix(c(1, 5, 8, 4, 5, 8, 4, NA, 8, 4, NA, NA), 4, 3)
+
+
+stopifnot(sum(res1, na.rm) == sum(gt1, na.rm))
+stopifnot(sum(is.na(res1)) == sum(is.na(gt1)))
+
+stopifnot(sum(res2, na.rm) == sum(gt2, na.rm))
+stopifnot(sum(is.na(res2)) == sum(is.na(gt2)))
+
