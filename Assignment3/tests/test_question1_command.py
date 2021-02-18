24a5a51644fd2978f91e64d7e5d9959faa7b92fc
Mason Tianyi Chen <39594659+masontchen@users.noreply.github.com>
2021-02-17 20:56:18 -0500

    Delete test_question1_command.py

--- Assignment3/tests/test_question1_command.py
+++
@@ -1,8 +0,0 @@
-import codecs
-import re
-
-
-if __name__ == "__main__":
-    f = codecs.open("q1.bat", "r", "utf-8").read()
-    command1 = re.compile("library(Hmisc)")
-    assert len(command1.findall(f)) == 0
