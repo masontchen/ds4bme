c1360b5410fa2eef82fa6576157784249c6334ef
Mason Tianyi Chen <39594659+masontchen@users.noreply.github.com>
2021-02-17 21:18:45 -0500

    Update test_question1_matrix.R

--- Assignment3/tests/test_question1_matrix.R
+++ Assignment3/tests/test_question1_matrix.R
@@ -1,3 +1,5 @@
+stopifnot(length(grep("Hmisc", readLines("question1.R"), value = TRUE)) == 0)
+
 source("question1.R")
 
 res1 = mylag(c(1, 5, 8, 4), c(1, 3))
@@ -12,4 +14,3 @@ stopifnot(sum(is.na(res1)) == sum(is.na(gt1)))
 
 stopifnot(sum(res2, na.rm) == sum(gt2, na.rm))
 stopifnot(sum(is.na(res2)) == sum(is.na(gt2)))
-
