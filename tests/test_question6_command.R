10c504784b1f10e4c49b998c8a0ac5bf75eb53c0
Mason Tianyi Chen <39594659+masontchen@users.noreply.github.com>
2021-02-19 12:55:33 -0500

    Update test_question6_command.R

--- tests/test_question6_command.R
+++ tests/test_question6_command.R
@@ -1,4 +1,4 @@
 stopifnot(sum(grep("birthday", readLines("question6.R"))) > 0)
 stopifnot(sum(grep("function()", readLines("question6.R"))) > 0)
 stopifnot(sum(grep("plot()", readLines("question6.R"))) > 0)
-stopifnot(sum(grep("for()", readLines("question6.R"))) > 0)
+stopifnot(sum(grep("for()", readLines("question6.R"))) + sum(grep("choose()", readLines("question6.R"))) > 0)
