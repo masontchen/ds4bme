7781f6249badaa94869bb9e1775fb89518015950
Mason Tianyi Chen <39594659+masontchen@users.noreply.github.com>
2021-02-18 20:27:07 -0500

    Update test_question6_command.R

--- Assignment3/tests/test_question6_command.R
+++ Assignment3/tests/test_question6_command.R
@@ -1,1 +1,4 @@
-stopifnot
+stopifnot(sum(grep("birthday", readLines("question6.R"))) > 0)
+stopifnot(sum(grep("function()", readLines("question6.R"))) > 0)
+stopifnot(sum(grep("plot()", readLines("question6.R"))) > 0)
+stopifnot(sum(grep("for()", readLines("question6.R"))) > 0)
