d032bd3d85b64a45f6a721d956902f3a42d03eca
Mason Tianyi Chen <39594659+masontchen@users.noreply.github.com>
2021-02-19 11:39:48 -0500

    Update test_question2_command.R

--- tests/test_question2_command.R
+++ tests/test_question2_command.R
@@ -1,2 +1,2 @@
-stopifnot(sum(grep("readLines", readLines("question1.R"))) > 0)
-stopifnot(sum(grep(".html", readLines("question1.R"))) > 0)
+stopifnot(sum(grep("readLines", readLines("question2.R"))) > 0)
+stopifnot(sum(grep(".html", readLines("question2.R"))) > 0)
