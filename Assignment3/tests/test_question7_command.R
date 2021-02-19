dc334686a6fbac3932d780800e6fea89f02e3ee4
Mason Tianyi Chen <39594659+masontchen@users.noreply.github.com>
2021-02-18 20:42:56 -0500

    Update test_question7_command.R

--- Assignment3/tests/test_question7_command.R
+++ Assignment3/tests/test_question7_command.R
@@ -1,1 +1,4 @@
-stopifnot(abs(gt - res) < 1e-10)
+stopifnot(sum(grep("ppv", readLines("question7.R"))) > 0)
+stopifnot(sum(grep("sensitivity", readLines("question7.R"))) > 0)
+stopifnot(sum(grep("specificity", readLines("question7.R"))) > 0)
+stopifnot(sum(grep("prevalence", readLines("question7.R"))) > 0)
