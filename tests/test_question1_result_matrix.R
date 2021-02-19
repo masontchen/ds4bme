9ce45b708cef994274d4298062ca2a574976565a
Mason Tianyi Chen <39594659+masontchen@users.noreply.github.com>
2021-02-19 11:53:06 -0500

    Update test_question1_result_matrix.R

--- tests/test_question1_result_matrix.R
+++ tests/test_question1_result_matrix.R
@@ -9,8 +9,8 @@ res2 = mylag(c(1, 5, 8, 4), c(-1, -2))
 gt2 = matrix(c(1, 5, 8, 4, 5, 8, 4, NA, 8, 4, NA, NA), 4, 3)
 
 
-stopifnot(sum(res1, na.rm) == sum(gt1, na.rm))
+stopifnot(sum(res1, na.rm=TRUE) == sum(gt1, na.rm=TRUE))
 stopifnot(sum(is.na(res1)) == sum(is.na(gt1)))
 
-stopifnot(sum(res2, na.rm) == sum(gt2, na.rm))
+stopifnot(sum(res2, na.rm=TRUE) == sum(gt2, na.rm=TRUE))
 stopifnot(sum(is.na(res2)) == sum(is.na(gt2)))
